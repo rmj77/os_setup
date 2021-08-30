08bb40e21fe8f859c94560a30e5bc5e9c027f1f8
pwablito <pwablito@gmail.com>
2021-08-29 22:34:04 -0600

    Fix syntax

--- setup.sh
+++ setup.sh
@@ -8,7 +8,7 @@ cd os_setup-master
 
 if [ "`uname`" == "Darwin" ]; then
     bash src/macos.sh
-elif [ "`uname`" == "Linux"]; then
+elif [ "`uname`" == "Linux" ]; then
     ### TODO Add more checks here for distros
     bash src/ubuntu.sh
 fi
