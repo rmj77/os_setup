ad0eadd5c6dfb9b3781d1fa1f5504284a2cd3fb7
pwablito <pwablito@gmail.com>
2021-08-30 00:13:31 -0600

    Syntax

--- src/gnome.sh
+++ src/gnome.sh
@@ -3,4 +3,4 @@
 set -e
 
 # Set favorite apps
-gsettings set org.gnome.shell favorite-apps "['firefox.desktop', 'thunderbird.desktop', 'code_code.desktop', 'org.gnome.Terminal.desktop', 'google-chrome.desktop]"
+gsettings set org.gnome.shell favorite-apps "['firefox.desktop', 'thunderbird.desktop', 'code_code.desktop', 'org.gnome.Terminal.desktop', 'google-chrome.desktop']"
