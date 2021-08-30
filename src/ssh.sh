9b17180f701a2bd4d1d95cf342a52bb39d790419
pwablito <pwablito@gmail.com>
2021-08-30 00:17:00 -0600

    Test if ssh keys exist

--- src/ssh.sh
+++ src/ssh.sh
@@ -2,6 +2,10 @@
 
 set -e
 
-ssh-keygen -t rsa -f ~/.ssh/id_rsa -N ''
+if [ -f ~/.ssh/id_rsa ]; then
+    echo "SSH keys already exists"
+else
+    ssh-keygen -t rsa -f ~/.ssh/id_rsa -N ''
+fi
 
 echo "Remember to add ssh keys to online git accounts"
