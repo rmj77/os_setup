168d7231d21b8d64edd6dc32133055e35d03d13c
pwablito <pwablito@gmail.com>
2021-08-29 22:32:16 -0600

    Add download instructions with repo-specific url

--- setup.sh
+++ setup.sh
@@ -1,3 +1,11 @@
+set -e
+
+cd `mktemp -d`
+wget https://github.com/pwablito/os_setup/archive/refs/heads/master.zip -O setup.zip
+unzip setup.zip
+cd os_setup-master
+
+
 if [ "`uname`" == "Darwin" ]; then
     bash src/macos.sh
 elif [ "`uname`" == "Linux"]; then
