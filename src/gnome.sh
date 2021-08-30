bce5922165de93a08e7909e5c9de7e1af8212482
pwablito <pwablito@gmail.com>
2021-08-30 00:12:10 -0600

    Gnome favorites

--- src/gnome.sh
+++ src/gnome.sh
@@ -1,1 +1,6 @@
-# TODO Add gnome setup commands here
+#!/bin/bash
+
+set -e
+
+# Set favorite apps
+gsettings set org.gnome.shell favorite-apps "['firefox.desktop', 'thunderbird.desktop', 'code_code.desktop', 'org.gnome.Terminal.desktop', 'google-chrome.desktop]"
