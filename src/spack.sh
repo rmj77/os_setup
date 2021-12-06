56bd08e636da43c1fd856410b27fe7a75860bb0f
Johnson, Richard Miles <rick_johnson@byu.edu>
2021-12-06 15:49:55 -0700

    Save to .bash_environment/spack.sh

--- src/spack.sh
+++ src/spack.sh
@@ -4,7 +4,7 @@ set -e
 
 cd ~
 git clone git@github.com:spack/spack.git
-echo "source ~/spack/share/spack/setup-env.sh" >> ~/.bashrc
+echo "source ~/spack/share/spack/setup-env.sh" >> ~/.bash_environment/spack.sh
 
 
 # TODO `spack install` some software
